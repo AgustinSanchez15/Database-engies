7048b4545d8fefec78959b26caf80a38f97af596
Agustin Sanchez <agustin.sanchez@upr.edu>
2021-04-12 22:31:22 -0400

    adding heroku

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,19 +1,6 @@
 pg_config = {
-    'user' : 'manuel',
-    'password' : 'manuel1',
-    'dbname' : 'db1',
-    'dbport' : 8083
-}
-# pg_config = {
-#     'user' : 'username',
-#     'password' : 'password',
-#     'dbname' : 'db1',
-#     'dbport' : 8083
-# }
-
-#pg_config = {
-#    'user': 'eeeprjerncitfb',
-#    'password': '0af8cb94aa683b204d19ce72c4ed5959d672ba8405cbd1e7ebda5ef2a0af5189',
-#    'dbname': 'difhr6768ef9a',
-#    'dbport': 5432
+    'user': 'eeeprjerncitfb',
+    'password': '0af8cb94aa683b204d19ce72c4ed5959d672ba8405cbd1e7ebda5ef2a0af5189',
+    'dbname': 'difhr6768ef9a',
+    'dbport': 5432
 #}
